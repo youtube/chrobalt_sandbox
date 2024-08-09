bcb6f0dd79fa07234e85d12fbe38090b16f99696
Kaido Kert <kaidokert@gmail.com>
2024-08-08 20:58:44 -0700

    Stub Starboard/Ozone layer

---
+++ ui/ozone/platform/starboard/starboard_ozone_ui_controls_test_helper.h
@@ -0,0 +1,16 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_PLATFORM_STARBOARD_STARBOARD_OZONE_UI_CONTROLS_TEST_HELPER_H_
+#define UI_OZONE_PLATFORM_STARBOARD_STARBOARD_OZONE_UI_CONTROLS_TEST_HELPER_H_
+
+#include "ui/ozone/public/ozone_ui_controls_test_helper.h"
+
+namespace ui {
+
+OzoneUIControlsTestHelper* CreateOzoneUIControlsTestHelperStarboard();
+
+}  // namespace ui
+
+#endif  // UI_OZONE_PLATFORM_STARBOARD_STARBOARD_OZONE_UI_CONTROLS_TEST_HELPER_H_
